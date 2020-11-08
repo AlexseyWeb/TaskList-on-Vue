a228e056695b17af4b505ba545c14ffdef61a102
alongenet <alongenet32@gmail.com>
2020-11-08 16:39:58 +0300

    default app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div>
+     <h1>{{header}}:</h1>
+      <input  type="text" class="task-input" v-model="task" v-on:keyup.enter="addTask(task)">
+      <button class="add-task" v-on:click="addTask(task)">Добавить</button>
+      <div class="item-task">
+          <ul>
+            <li 
+            v-for="(task, index) in tasks" 
+            :key="task">
+            <input type="checkbox" 
+            v-on:click="removeTask(index)">
+            {{task}}
+            </li>
+          </ul>
+      </div>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
+
   name: 'App',
   components: {
-    HelloWorld
+   
+  },
+  
+  data(){
+    return {
+      task: '',
+      header: 'Список дел',
+      tasks: ['Бросить пить энергетики', 'бросить курить', 'занятся спортом']
+    }
+  },
+
+  methods: {
+    addTask(task){
+      this.tasks.push(task);
+      this.task = '';
+      
+      
+    },
+    removeTask(index){
+      this.tasks.splice(index, 1);
+    }
   }
 }
 </script>
@@ -23,4 +58,46 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+h1{
+  padding-bottom: 30px;
+  color: white;
+  text-align: center;
+}
+
+.item-task {
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 22px;
+  color: white;
+}
+
+.item-task > ul {
+  list-style-type: none;
+}
+.item-task > ul > li:hover{
+  text-decoration: line-through;
+  color: red;
+}
+input[type="text"]{
+  width: 400px;
+  height: 50px;
+  font-size: 22px;
+}
+input[type="text"]:focus{
+  border:darkcyan;
+  outline: none;
+}
+button{
+  width: 200px;
+  height: 50px;
+  font-size: 22px;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  border: none;
+  outline: none;
+}
+
 </style>
